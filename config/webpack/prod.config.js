e773823baf34b043eb93e81c9cf28472850350d9
Leonardo Manrique <the.first.leomestizo@gmail.com>
2019-12-20 02:05:50 -0300

    Add '_redirects' file to fix issue when refreshing the page in a route

--- config/webpack/prod.config.js
+++ config/webpack/prod.config.js
@@ -3,9 +3,13 @@ const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlPlugin = require("html-webpack-plugin");
 const HtmlRootPlugin = require("html-webpack-root-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 const webpackMerge = require("webpack-merge");
 
 const baseConfig = require("./base.config");
+
+const workingDirectory = process.cwd();
 
 module.exports = webpackMerge(baseConfig, {
   mode: "production",
@@ -47,6 +51,12 @@ module.exports = webpackMerge(baseConfig, {
     ],
   },
   plugins: [
+    new CopyPlugin([
+      {
+        from: path.resolve(workingDirectory, "_redirects"),
+        to: path.resolve(workingDirectory, "dist"),
+      },
+    ]),
     new HtmlPlugin({
       title: "Deviget code challenge",
     }),
